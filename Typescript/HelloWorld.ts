d8a577cec7e5a408ac23a9a41f5510d8fd9ae5eb
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-07-29 16:19:47 +0100

    completed hello world

---
+++ Typescript/HelloWorld.ts
@@ -0,0 +1,9 @@
+/*Modify the provided code to it produces the code hello world!
+export function hello(): string {
+  return 'Goodbye, Mars!'
+}
+*/
+export function hello(): string {
+    return 'Hello, World!'
+}
+  
